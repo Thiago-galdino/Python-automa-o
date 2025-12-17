26e60f792568be5c4bc4b885239ec20d1a3e2ccc
GaldinoSousa <galdinosousa336@gmail.com>
2025-12-17 14:45:16 -0300

    mudanças

--- 1-PythonFund/6-strings.py
+++ 1-PythonFund/6-strings.py
@@ -3,4 +3,9 @@ gameName2 = "fifa 23"
 
 print(gameName)
 print(gameName2)
-print(gameName == gameName2)
+print(gameName == gameName2) #Python é case-sensitive
+gameDescription = '''
+FIFA 23 é um jogo eletrônico de simulação de futebol
+desenvolvido pela EA Vancouver e EA Romania e publicado pela Electronic Arts.
+É o 30º título principal da série FIFA da EA Sports e foi lançado em 30 de setembro de 2022 para PlayStation 4, PlayStation 5, Xbox One, Xbox Series X/S, Microsoft Windows, Google Stadia e Nintendo Switch.'''
+print(gameDescription)
