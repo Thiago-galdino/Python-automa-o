a44b93edd84c27b4592ea96ef50416c64fdff905
GaldinoSousa <galdinosousa336@gmail.com>
2025-12-17 21:41:01 -0300

    atualizar

--- 1-PythonFund/8-slice.py
+++ 1-PythonFund/8-slice.py
@@ -3,3 +3,8 @@ gameDescription = '''
 Fifa 23 é um jogo eletrônico de simulação de futebol
 desenvolvido pela EA Vancouver e EA Romania e publicado pela Electronic Arts.
 '''
+
+# string[início:fim:] - indice começa na posição 0 | indice final e -1 
+
+# 1 - Busque toda string a partir da primeira posição
+print(gameName[0:]) # Fifa 23
